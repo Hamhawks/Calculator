ee9faa8ff98d0e172f42b9cbede2e26fa0bf2772
Hamhawks <wood.jon.chris@gmail.com>
2020-10-06 14:50:47 -0500

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="calculator2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculator</title>
+    <script src="modernizer-1.5.js"></script>
+    <link rel="stylesheet" href="calculator2.css" title="Stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="displayLbl" runat="server" Width="200px"></asp:Label>
+        </div>
+        <p>
+            <asp:Button ID="btnSeven" runat="server" OnClick="btnSeven_Click" Text="7" Width="50px" />
+            <asp:Button ID="btnEight" runat="server" OnClick="btnEight_Click" Text="8" Width="50px" />
+            <asp:Button ID="btnNine" runat="server" OnClick="btnNine_Click" Text="9" Width="50px" />
+            <asp:Button ID="btnDivide" runat="server" OnClick="btnDivide_Click" Text="/" Width="50px" />
+        </p>
+        <asp:Button ID="btnFour" runat="server" OnClick="btnFour_Click" Text="4" Width="50px" />
+        <asp:Button ID="btnFive" runat="server" OnClick="btnFive_Click" Text="5" Width="50px" />
+        <asp:Button ID="btnSix" runat="server" OnClick="btnSix_Click" Text="6" Width="50px" />
+        <asp:Button ID="btnMultiply" runat="server" OnClick="btnMultiply_Click" Text="*" Width="50px" />
+        <p>
+            <asp:Button ID="btnOne" runat="server" OnClick="btnOne_Click" Text="1" Width="50px" />
+            <asp:Button ID="btnTwo" runat="server" OnClick="btnTwo_Click" Text="2" Width="50px" />
+            <asp:Button ID="btnThree" runat="server" OnClick="btnThree_Click" Text="3" Width="50px" />
+            <asp:Button ID="btnMinus" runat="server" OnClick="btnMinus_Click" Text="-" Width="50px" />
+        </p>
+        <asp:Button ID="btnZero" runat="server" OnClick="btnZero_Click" Text="0" Width="100px" />
+        <asp:Button ID="btnEqual" runat="server" OnClick="btnEquals_Click" Text="=" Width="50px" />
+        <asp:Button ID="btnPlus" runat="server" OnClick="btnPlus_Click" Text="+" Width="50px" />
+        <p>
+            <asp:Button ID="btnClear" runat="server" OnClick="btnClear_Click" Text="Clear" Width="100px" />
+            <asp:Button ID="btnRecall" runat="server" Text="Recall" Width="100px" OnClick="btnRecall_Click" />
+        </p>
+        <asp:Label ID="recallLbl" runat="server" Width="200px"></asp:Label>
+
+    </form>
+    </body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace calculator2
+{
+
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static String firstNum;
+        static String secondNum;
+        static char sign;
+        static double calc;
+        static bool error = false;
+        static String equations = "";
+        static int identification;
+
+        static SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-58QP3EG\SQL;Initial Catalog=Recall2;Integrated Security=True");
+       
+       
+        
+        
+        
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSeven_Click(object sender, EventArgs e)
+        {
+            if (sign == '/' || sign == '*' || sign == '-' || sign == '+' || error == true)
+            {
+                displayLbl.Text = "";
+            }
+            displayLbl.Text += "7";
+        }
+        protected void btnEight_Click(object sender, EventArgs e)
+        {
+            if (sign == '/' || sign == '*' || sign == '-' || sign == '+' || error == true)
+            {
+                displayLbl.Text = "";
+            }
+            displayLbl.Text += "8";
+        }
+        protected void btnNine_Click(object sender, EventArgs e)
+        {
+            if (sign == '/' || sign == '*' || sign == '-' || sign == '+' || error == true)
+            {
+                displayLbl.Text = "";
+            }
+            displayLbl.Text += "9";
+        }
+        protected void btnFour_Click(object sender, EventArgs e)
+        {
+            if (sign == '/' || sign == '*' || sign == '-' || sign == '+' || error == true)
+            {
+                displayLbl.Text = "";
+            }
+            displayLbl.Text += "4";
+        }
+        protected void btnFive_Click(object sender, EventArgs e)
+        {
+            if (sign == '/' || sign == '*' || sign == '-' || sign == '+' || error == true)
+            {
+                displayLbl.Text = "";
+            }
+            displayLbl.Text += "5";
+        }
+        protected void btnSix_Click(object sender, EventArgs e)
+        {
+            if (sign == '/' || sign == '*' || sign == '-' || sign == '+' || error == true)
+            {
+                displayLbl.Text = "";
+            }
+            displayLbl.Text += "6";
+        }
+        protected void btnOne_Click(object sender, EventArgs e)
+        {
+            if (sign == '/' || sign == '*' || sign == '-' || sign == '+' || error == true)
+            {
+                displayLbl.Text = "";
+            }
+            displayLbl.Text += "1";
+        }
+        protected void btnTwo_Click(object sender, EventArgs e)
+        {
+            if (sign == '/' || sign == '*' || sign == '-' || sign == '+' || error == true)
+            {
+                displayLbl.Text = "";
+            }
+            displayLbl.Text += "2";
+        }
+        protected void btnThree_Click(object sender, EventArgs e)
+        {
+            if (sign == '/' || sign == '*' || sign == '-' || sign == '+' || error == true)
+            {
+                displayLbl.Text = "";
+            }
+            displayLbl.Text += "3";
+        }
+        protected void btnZero_Click(object sender, EventArgs e)
+        {
+            if (sign == '/' || sign == '*' || sign == '-' || sign == '+' || error == true)
+            {
+                displayLbl.Text = "";
+            }
+            displayLbl.Text += "0";
+        }
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            displayLbl.Text = "";
+            sign = ' ';
+            firstNum = "";
+            secondNum = "";
+            recallLbl.Text = "";
+        }
+        protected void btnDivide_Click(object sender, EventArgs e)
+        {
+            firstNum = displayLbl.Text;
+            displayLbl.Text = "/";
+            sign = '/';
+           
+            
+        }
+        protected void btnMultiply_Click(object sender, EventArgs e)
+        {
+            firstNum = displayLbl.Text;
+            displayLbl.Text = "*";
+            sign = '*';
+        }
+        protected void btnMinus_Click(object sender, EventArgs e)
+        {
+            firstNum = displayLbl.Text;
+            displayLbl.Text = "-";
+            sign = '-';
+            
+            
+        }
+        protected void btnPlus_Click(object sender, EventArgs e)
+        {
+            firstNum = displayLbl.Text;
+            displayLbl.Text = "+";
+            sign = '+';
+        }
+        protected void btnEquals_Click(object sender, EventArgs e)
+        {
+            secondNum = displayLbl.Text;
+            if(secondNum == "0" && sign == '/')
+            {
+                displayLbl.Text = "Error cannot divide by 0";
+                firstNum = "";
+                secondNum = "";
+                sign = ' ';
+                error = true;
+            }
+            else
+            {
+
+                
+                switch (sign)
+                {
+                    case '/':
+                        calc = double.Parse(firstNum) / double.Parse(secondNum);
+                        break;
+                    case '*':
+                        calc = double.Parse(firstNum) * double.Parse(secondNum);
+                        break;
+                    case '-':
+                        calc = double.Parse(firstNum) - double.Parse(secondNum);
+                        break;
+                    case '+':
+                        calc = double.Parse(firstNum) + double.Parse(secondNum);
+                        break;
+                        
+                        ;
+                }
+                equations= firstNum.ToString() + sign.ToString() + secondNum.ToString();
+                displayLbl.Text = calc.ToString();
+
+                SqlCommand commI = new SqlCommand("SELECT TOP 1 ID FROM Table_Recall ORDER BY ID DESC;", con);
+               try
+                {
+                    con.Open();
+                    SqlDataReader reader = commI.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        var id = reader["ID"].ToString();
+                        identification = Int32.Parse(id)+1;
+                        
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    displayLbl.Text = "Error";
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+                SqlCommand commF = new SqlCommand("INSERT INTO Table_Recall(equation, answer, ID) VALUES(\'"+ equations + "\',\'" + calc + "\'," + identification +");", con);
+                try
+                {
+                    con.Open();
+                    commF.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    displayLbl.Text = "Error";
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+
+
+
+
+            }
+           
+
+        }
+       
+        protected void btnRecall_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-58QP3EG\SQL;Initial Catalog=Recall2;Integrated Security=True");
+            SqlCommand comm = new SqlCommand("SELECT TOP 1 answer FROM Table_Recall ORDER BY ID DESC;", conn);
+            SqlCommand commE = new SqlCommand("SELECT TOP 1 equation FROM Table_Recall ORDER BY ID DESC; ", conn);
+
+            
+            
+            
+            
+
+            try
+            {
+                conn.Open();
+                
+                SqlDataReader reader = comm.ExecuteReader();
+                while (reader.Read())
+                {
+                    var read = reader["answer"].ToString();
+                    displayLbl.Text = read.ToString();
+                    
+                }
+                
+            }
+            catch (SqlException ex)
+            {
+                displayLbl.Text = "Error";
+            }
+            finally
+            {
+                
+                conn.Close();
+               
+            }
+          
+            try
+            {
+                conn.Open();
+                SqlDataReader readerE = commE.ExecuteReader();// here seems to be a problem
+                while (readerE.Read())
+                {
+                    var read = readerE["equation"].ToString();
+                    recallLbl.Text = read.ToString();
+                    
+                }
+            }
+            catch(SqlException dx)
+            {
+                recallLbl.Text = "Error";
+                
+            }
+            finally
+            {
+                conn.Close();
+            }
+         
+
+
+
+        }
+    }
+}
